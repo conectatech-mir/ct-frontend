777f71132a625e6789f7e5d4885de73a9f0121e9
GiAn <gian.gavins@gmail.com>
2022-03-16 23:15:44 -0500

    v1.0.0: Add user and paswd catch

--- src/pages/login/Login.component.jsx
+++ src/pages/login/Login.component.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useRef, useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LockClosedIcon } from '@heroicons/react/solid';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
   let navigate = useNavigate();
+
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(()=> {
+    setErrMsg('');
+  }, [user, pwd]);
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setSuccess(true);
     console.log("Presionando el botón de Login");
+    console.log("usuario: " + user + "Password: " + pwd);
     navigate("/profesionalHomePage");
   };
 
@@ -48,81 +70,90 @@ const Login = () => {
             </a>
           </p>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" defaultValue="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-              />
-              <label
-                htmlFor="remember-me"
-                className="ml-2 block text-sm text-gray-200"
-              >
-                Remember me
-              </label>
-            </div>
-
-              <p className="mt-2 text-center text-sm text-gray-200">
-                O{' '}
-                <Link to="/registro">Crea tu cuenta</Link>
-              </p>
-          </div>
-          <div>
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <LockClosedIcon
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  aria-hidden="true"
+        <section>
+          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  ref={userRef}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                  onChange={(e) => setUser(e.target.value)}
+                  value={user}
                 />
-              </span>
-              Login
-            </button>
-          </div>
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  onChange={(e) => setPwd(e.target.value)}
+                  value={pwd}
+                />
+              </div>
+            </div>
 
-          <p className="mt-2 text-center text-sm text-gray-200">
-            O{" "}
-            <Link to="/registro">Crea tu cuenta</Link>
-          </p>
-        </form>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-200"
+                >
+                  Remember me
+                </label>
+              </div>
+
+                <p className="mt-2 text-center text-sm text-gray-200">
+                  O{' '}
+                  <Link to="/registro">Crea tu cuenta</Link>
+                </p>
+            </div>
+            <div>
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Login
+              </button>
+            </div>
+
+            <p className="mt-2 text-center text-sm text-gray-200">
+              O{" "}
+              <Link to="/registro">Crea tu cuenta</Link>
+            </p>
+          </form>
+
+        </section>
       </div>
     </div>
   );
