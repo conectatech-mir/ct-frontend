78abcf0d4631a5156f25e42503455e15ab74ade2
Junior Almeyda <jr.almeyda7@gmail.com>
2022-04-09 01:29:23 -0500

    añadiendo hook para formulario de registro

--- src/pages/registro/Registro.component.jsx
+++ src/pages/registro/Registro.component.jsx
@@ -1,56 +1,130 @@
-import './Registro.css'
+import "./Registro.css";
+import useForm from "../../hooks/useForm";
 
-const Registro = ()=> {
-  return(
+const Registro = () => {
+  const initialForm = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cellphone: "",
+    typeUser: "",
+  };
+  const [formValues, handleInputChange, reset] = useForm(initialForm);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      "🚀 ~ file: Registro.component.jsx ~ line 14 ~ Registro ~ formValues",
+      formValues
+    );
+  };
+  return (
     <div className="w-7/12 mx-auto">
-      <form className='p-8 pb-0'>
-        <h2 className='tituloRegistro font-black'>Regístrate, y así podrás ser parte de nuestra comunidad.</h2>
+      <form className="p-8 pb-0" onSubmit={handlerSubmit}>
+        <h2 className="tituloRegistro font-black">
+          Regístrate, y así podrás ser parte de nuestra comunidad.
+        </h2>
         <div className="form-control">
           <label className="label">
             <span className="label-text labelSpan font-medium">Nombres</span>
-          </label> 
-          <input placeholder="Ingrese sus nombres" className="input input-lg input-bordered" type="text"></input>
+          </label>
+          <input
+            placeholder="Ingrese sus nombres"
+            className="input input-lg input-bordered"
+            type="text"
+            name="firstName"
+            value={formValues.firstName}
+            onChange={handleInputChange}
+          ></input>
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text labelSpan font-medium">Apellidos</span>
-          </label> 
-          <input placeholder="Ingrese sus apellidos" className="input input-lg input-bordered" type="text"></input>
+          </label>
+          <input
+            placeholder="Ingrese sus apellidos"
+            className="input input-lg input-bordered"
+            type="text"
+            name="lastName"
+            value={formValues.lastName}
+            onChange={handleInputChange}
+          ></input>
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text labelSpan font-medium">Correo</span>
-          </label> 
-          <input placeholder="Ingrese su correo" className="input input-lg input-bordered" type="text"></input>
+          </label>
+          <input
+            placeholder="Ingrese su correo"
+            className="input input-lg input-bordered"
+            type="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleInputChange}
+          ></input>
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text labelSpan font-medium">Contraseña</span>
-          </label> 
-          <input placeholder="Ingrese su contraseña" className="input input-lg input-bordered" type="password"></input>
+          </label>
+          <input
+            placeholder="Ingrese su contraseña"
+            className="input input-lg input-bordered"
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleInputChange}
+            autoComplete="true"
+          ></input>
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text labelSpan font-medium">Celular</span>
-          </label> 
-          <input placeholder="Ingrese su celular" className="input input-lg input-bordered" type="text"></input>
+          </label>
+          <input
+            placeholder="Ingrese su celular"
+            className="input input-lg input-bordered"
+            type="text"
+            name="cellphone"
+            value={formValues.cellphone}
+            onChange={handleInputChange}
+          ></input>
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text labelSpan font-medium">Tipo de Usuario</span>
+            <span className="label-text labelSpan font-medium">
+              Tipo de Usuario
+            </span>
           </label>
-          <select className="select select-bordered select-lg w-full max-w-xs">
-            <option className='text-lg' disabled="disabled" selected="selected">Seleccione Tipo de Usuario</option> 
-            <option className='text-lg'>Usuario</option> 
-            <option className='text-lg'>Profesional</option>
+          <select
+            className="select select-bordered select-lg w-full max-w-xs"
+            name="typeUser"
+            onChange={handleInputChange}
+            defaultValue=""
+          >
+            <option className="text-lg" disabled="disabled">
+              Seleccione Tipo de Usuario
+            </option>
+            <option className="text-lg">Usuario</option>
+            <option className="text-lg">Profesional</option>
           </select>
         </div>
         <div className="flex justify-center">
-          <input type="submit" className="btn w-2/5 m-5" value="Cancelar"></input>
-          <input type="submit" className="btn btn-info w-2/5 m-5" value="Registrar"></input>
+          <input
+            type="button"
+            className="btn w-2/5 m-5"
+            value="Cancelar"
+            onClick={() => console.log("cancelar")}
+          ></input>
+          <input
+            type="submit"
+            className="btn btn-info w-2/5 m-5"
+            value="Registrar"
+          ></input>
         </div>
       </form>
     </div>
   );
-}
+};
 export default Registro;
